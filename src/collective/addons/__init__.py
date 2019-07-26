6e116710b067de9337b832f4389ea112606b2895
andreasma <maand@gmx.de>
2019-07-26 12:23:12 +0200

    Add function quote_chars.

--- src/collective/addons/__init__.py
+++ src/collective/addons/__init__.py
@@ -4,3 +4,17 @@ from zope.i18nmessageid import MessageFactory
 
 
 _ = MessageFactory('collective.addons')
+
+
+MULTISPACE = u'\u3000'
+
+
+def quote_chars(value):
+    # We need to quote parentheses when searching text indices
+    if '(' in value:
+        value = value.replace('(', '"("')
+    if ')' in value:
+        value = value.replace(')', '")"')
+    if MULTISPACE in value:
+        value = value.replace(MULTISPACE, ' ')
+    return value
