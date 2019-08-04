b32fc681afc9849fa2ac50a8e211eb8379f20460
andreasma <maand@gmx.de>
2019-08-04 14:56:13 +0200

    Use @implementer instead of implements

--- src/collective/addons/releasecustomurl.py
+++ src/collective/addons/releasecustomurl.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 from plone.app.content.interfaces import INameFromTitle
-from zope.interface import implements
+from zope.interface import implementer
 
 
 class INameForReleaseURL(INameFromTitle):
     def title():
         """Return a processed title"""
 
-
+@implementer(INameForReleaseURL)
 class NameForReleaseURL(object):
-    implements(INameForReleaseURL)
 
     def __init__(self, context):
         self.context = context
