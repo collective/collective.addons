847e92b9ec1c2d539f01d616a1564920d4a79775
andreasma <maand@gmx.de>
2019-08-17 14:50:09 +0200

    Add notifications module.

---
+++ src/collective/addons/notifications.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from collective.addons.addoncenter import IAddonCenter
+from plone import api
+
+
+def notifiyAboutNewVersion(addonproject, event):
+    if getattr(event, 'descriptions') and event.descriptions:
+        for d in event.descriptions:
+            if getattr(d, 'interface') and d.interface is IAddonCenter and \
+                    'available_versions' in d.attributes:
+                users = api.user.get_users()
+                message = 'We added a new version of the product to the ' \
+                          'list.\n Please add this version to your ' \
+                          'template project(s), if it is (they ' \
+                          'are) compatible with this version.\n\n' \
+                          'You could do this on your project(s). Go to ' \
+                          'your project and choose the command ' \
+                          '\'edit\' from the menu bar. Go to the section ' \
+                          '\'compatible with versions of the product\' ' \
+                          'and mark the checkbox for the new version of ' \
+                          'the product.\n\n' \
+                          'Kind regards,\n\n' \
+                          'Administration Team'
+                for f in users:
+                    mailaddress = f.getProperty('email')
+                    api.portal.send_email(
+                        recipient=mailaddress,
+                        sender=api.portal.get_registry_record(
+                            'plone.email_from_address'),
+                        subject='New Version of the Product Added',
+                        body=message,
+                    )
