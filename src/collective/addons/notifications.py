b7524ca4c970dfb9f847cf04ea03768b054fc90e
andreasma <maand@gmx.de>
2019-08-28 18:14:21 +0200

    Fixed the notifications function.

--- src/collective/addons/notifications.py
+++ src/collective/addons/notifications.py
@@ -6,7 +6,7 @@ from plone import api
 def notifiyAboutNewVersion(addonproject, event):
     if hasattr(event, 'descriptions') and event.descriptions:
         for d in event.descriptions:
-            if hasattr(d, 'interface') and d.interface is IEUpCenter and \
+            if hasattr(d, 'interface') and d.interface is IAddonCenter and \
                 'available_versions' in d.attributes:
                 users = api.user.get_users()
                 message = 'We added a new version of the product to the ' \
