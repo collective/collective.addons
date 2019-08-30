91b62449a3cfc638eeb2480b3659408ab1be44a5
andreasma <maand@gmx.de>
2019-08-30 19:53:46 +0200

    Fix the message to the sender.

--- src/collective/addons/mailtoauthor.py
+++ src/collective/addons/mailtoauthor.py
@@ -185,7 +185,7 @@ class MailToAuthorForm(AutoExtensibleForm, form.Form):
 
         api.portal.show_message(
             message=_(safe_unicode('We send your message to the author '
-                                   "of the project. It's on his choice, "
+                                   "of the project. It's on her / his choice, "
                                    "if she'll / he'll get back to you.")),
             request=self.request,
             type='info')
