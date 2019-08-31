eaf3b4310a97b12b65ab8ee3543411a7789f29a9
andreasma <maand@gmx.de>
2019-08-31 17:59:19 +0200

    Send notifications only to the project email address and not to all users of the Plone instance.

--- src/collective/addons/notifications.py
+++ src/collective/addons/notifications.py
@@ -8,7 +8,8 @@ def notifiyAboutNewVersion(addonproject, event):
         for d in event.descriptions:
             if hasattr(d, 'interface') and d.interface is IAddonCenter and \
                 'available_versions' in d.attributes:
-                users = api.user.get_users()
+                catalog = api.portal.get_tool(name='portal_catalog')
+                projectemail = catalog.uniqueValuesFor('addonprojectcontact')
                 message = 'We added a new version of the product to the ' \
                           'list.\n Please add this version to your ' \
                           'template project(s), if it is (they ' \
@@ -21,8 +22,8 @@ def notifiyAboutNewVersion(addonproject, event):
                           'the product.\n\n' \
                           'Kind regards,\n\n' \
                           'Administration Team'
-                for f in users:
-                    mailaddress = f.getProperty('email')
+                for f in projectemail:
+                    mailaddress = f
                     api.portal.send_email(
                         recipient=mailaddress,
                         sender=api.portal.get_registry_record(
