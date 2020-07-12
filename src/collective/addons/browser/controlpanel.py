8ca2330444d65b1f729cadbdd3d77ce869a9aecd
andreasma <maand@gmx.de>
2020-07-12 20:33:29 +0200

    Add a controlpanel and a registry record. First step to move general configuration field from addon center to configuration panel.

---
+++ src/collective/addons/browser/controlpanel.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from collective.addons import _
+from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+from plone.app.registry.browser.controlpanel import RegistryEditForm
+from plone.z3cform import layout
+from zope import schema
+from zope.interface import Interface
+
+
+class ICollectiveaddonsControlPanel(Interface):
+
+    available_category = schema.Tuple(
+        title=_(u'Available Categories'),
+        value_type=schema.TextLine(),
+    )
+
+
+class CollectiveaddonsControlPanelForm(RegistryEditForm):
+    schema = ICollectiveaddonsControlPanel
+    schema_prefix = 'collectiveaddons'
+    label = u'Collective Addons Settings'
+
+CollectiveaddonsControlPanelView = layout.wrap_form(
+    CollectiveaddonsControlPanelForm, ControlPanelFormWrapper)
