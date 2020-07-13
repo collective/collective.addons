1451c18d773e90fafd24edf3b9d0c7062ca391d9
andreasma <maand@gmx.de>
2020-07-13 20:31:47 +0200

    Add a first vocabulary.

---
+++ src/collective/addons/vocabularies.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from plone import api
+from plone.app.vocabularies.terms import safe_simplevocabulary_from_values
+from zope.interface import provider
+from zope.schema.interfaces import IVocabularyFactory
+
+
+@provider(IVocabularyFactory)
+def CategoriesVocabularyFactory(context):
+    values = api.portal.get_registry_record('collectiveaddons.available_category')
+    return safe_simplevocabulary_from_values(values)
