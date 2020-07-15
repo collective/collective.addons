7c58fdbfeba2112af4d83fc7451cf43fd3852b49
andreasma <maand@gmx.de>
2020-07-15 20:57:24 +0200

    Add further vocabularies.

--- src/collective/addons/vocabularies.py
+++ src/collective/addons/vocabularies.py
@@ -15,3 +15,15 @@ def CategoriesVocabularyFactory(context):
 def LicensesVocabularyFactory(context):
     values = api.portal.get_registry_record('collectiveaddons.available_licenses')
     return safe_simplevocabulary_from_values(values)
+
+
+@provider(IVocabularyFactory)
+def VersionsVocabularyFactory(context):
+    values = api.portal.get_registry_record('collectiveaddons.available_versions')
+    return safe_simplevocabulary_from_values(values)
+
+
+@provider(IVocabularyFactory)
+def PlatformVocabularyFactory(context):
+    values = api.portal.get_registry_record('collectiveaddons.available_platforms')
+    return safe_simplevocabulary_from_values(values)
