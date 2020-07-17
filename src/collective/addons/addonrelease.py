ef5f10ed21f5a2ce2192c75a0cd5e0c10c4a84a0
andreasma <maand@gmx.de>
2020-07-17 20:12:50 +0200

    Remove unused imports.

--- src/collective/addons/addonrelease.py
+++ src/collective/addons/addonrelease.py
@@ -15,14 +15,11 @@ from Products.validation import V_REQUIRED  # noqa
 from z3c.form import validator
 from z3c.form.browser.checkbox import CheckBoxFieldWidget
 from zope import schema
-from zope.interface import directlyProvides
 from zope.interface import Invalid
 from zope.interface import invariant
 from zope.interface import provider
 from zope.schema.interfaces import IContextAwareDefaultFactory
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleTerm
-from zope.schema.vocabulary import SimpleVocabulary
+
 
 import itertools
 import re
