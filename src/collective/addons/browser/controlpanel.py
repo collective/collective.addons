653581b9a8d2e25e73cf2190bffd8ac7238d35fa
andreasma <maand@gmx.de>
2020-07-21 19:13:39 +0200

    Add fields for legal disclaimer to the controlpanel.

--- src/collective/addons/browser/controlpanel.py
+++ src/collective/addons/browser/controlpanel.py
@@ -2,6 +2,7 @@
 from collective.addons import _
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
 from plone.app.registry.browser.controlpanel import RegistryEditForm
+from plone.supermodel import model
 from plone.z3cform import layout
 from zope import schema
 from zope.interface import Interface
@@ -55,6 +56,13 @@ class ICollectiveaddonsControlPanel(Interface):
                                        value_type=schema.TextLine(),
                                        )
 
+    model.fieldset('fileextensions',
+                   label=u'Allowed File Extensions',
+                   fields=['allowed_addonfileextension',
+                           'allowed_apimageextension',
+                           'allowed_apdocfileextensions'],
+                   )
+
     allowed_addonfileextension = schema.TextLine(
         title=_(u'Allowed add-on file extensions'),
         description=_(u'Fill in the allowed file extensions for add-ons, seperated by '
@@ -74,6 +82,46 @@ class ICollectiveaddonsControlPanel(Interface):
         description=_(u'Fill in the allowed documentation file extensions, '
                       u"seperated by a pipe '|'."),
         default='pdf|odt',
+    )
+
+    model.fieldset('disclaimer',
+                   label=u'Legal Disclaimer',
+                   fields=['title_legaldisclaimer', 'legal_disclaimer',
+                           'title_legaldownloaddisclaimer',
+                           'legal_downloaddisclaimer'],
+                   )
+
+    title_legaldisclaimer = schema.TextLine(
+        title=_(u'Title for Legal Disclaimer and Limitations'),
+        default=_(u'Legal Disclaimer and Limitations'),
+        required=False,
+    )
+
+    legal_disclaimer = schema.Text(
+        title=_(u'Text of the Legal Disclaimer and Limitations'),
+        description=_(u'Enter the text of the legal disclaimer and '
+                      u'limitations that should be displayed to the '
+                      u'project creator and should be accepted by '
+                      u'the owner of the project.'),
+        default=_(u'Fill in the legal disclaimer, that had to be '
+                  u'accepted by the project owner.'),
+        required=False,
+    )
+
+    title_legaldownloaddisclaimer = schema.TextLine(
+        title=_(
+            u'Title of the Legal Disclaimer and Limitations for Downloads'),
+        default=_(u'Legal Disclaimer and Limitations for Downloads'),
+        required=False,
+    )
+
+    legal_downloaddisclaimer = schema.Text(
+        title=_(u'Text of the Legal Disclaimer and Limitations for Downlaods'),
+        description=_(u'Enter any legal disclaimer and limitations for '
+                      u'downloads that should appear on each page for '
+                      u'dowloadable files.'),
+        default=_(u'Fill in the text for the legal download disclaimer.'),
+        required=False,
     )
 
 
