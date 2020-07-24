098a150d3e8499eee5283492347626cb90c170fa
andreasma <maand@gmx.de>
2020-07-24 10:27:48 +0200

    Use safe_unicode for further strings and isort fix.

--- src/collective/addons/browser/controlpanel.py
+++ src/collective/addons/browser/controlpanel.py
@@ -73,56 +73,56 @@ class ICollectiveaddonsControlPanel(Interface):
     )
 
     allowed_apimageextension = schema.TextLine(
-        title=_(u'Allowed image file extension'),
-        description=_(u'Fill in the allowed image file extensions, seperated '
-                      u"by a pipe '|'."),
-        default='jpg|jpeg|png|gif',
+        title=_(safe_unicode('Allowed image file extension')),
+        description=_(safe_unicode('Fill in the allowed image file extensions, seperated '
+                      "by a pipe '|'.")),
+        default=safe_unicode('jpg|jpeg|png|gif'),
     )
 
     allowed_apdocfileextensions = schema.TextLine(
-        title=_(u'Allowed documentation file extension'),
-        description=_(u'Fill in the allowed documentation file extensions, '
-                      u"seperated by a pipe '|'."),
-        default='pdf|odt',
+        title=_(safe_unicode('Allowed documentation file extension')),
+        description=_(safe_unicode('Fill in the allowed documentation file extensions, '
+                      "seperated by a pipe '|'.")),
+        default=safe_unicode('pdf|odt'),
     )
 
     model.fieldset('disclaimer',
-                   label=u'Legal Disclaimer',
+                   label=safe_unicode('Legal Disclaimer'),
                    fields=['title_legaldisclaimer', 'legal_disclaimer',
                            'title_legaldownloaddisclaimer',
                            'legal_downloaddisclaimer'],
                    )
 
     title_legaldisclaimer = schema.TextLine(
-        title=_(u'Title for Legal Disclaimer and Limitations'),
-        default=_(u'Legal Disclaimer and Limitations'),
+        title=_(safe_unicode('Title for Legal Disclaimer and Limitations')),
+        default=_(safe_unicode('Legal Disclaimer and Limitations')),
         required=False,
     )
 
     legal_disclaimer = schema.Text(
-        title=_(u'Text of the Legal Disclaimer and Limitations'),
-        description=_(u'Enter the text of the legal disclaimer and '
-                      u'limitations that should be displayed to the '
-                      u'project creator and should be accepted by '
-                      u'the owner of the project.'),
-        default=_(u'Fill in the legal disclaimer, that had to be '
-                  u'accepted by the project owner.'),
+        title=_(safe_unicode('Text of the Legal Disclaimer and Limitations')),
+        description=_(safe_unicode('Enter the text of the legal disclaimer and '
+                      'limitations that should be displayed to the '
+                      'project creator and should be accepted by '
+                      'the owner of the project.')),
+        default=_(safe_unicode('Fill in the legal disclaimer, that had to be '
+                  'accepted by the project owner.')),
         required=False,
     )
 
     title_legaldownloaddisclaimer = schema.TextLine(
         title=_(
-            u'Title of the Legal Disclaimer and Limitations for Downloads'),
-        default=_(u'Legal Disclaimer and Limitations for Downloads'),
+            safe_unicode('Title of the Legal Disclaimer and Limitations for Downloads')),
+        default=_(safe_unicode('Legal Disclaimer and Limitations for Downloads')),
         required=False,
     )
 
     legal_downloaddisclaimer = schema.Text(
-        title=_(u'Text of the Legal Disclaimer and Limitations for Downlaods'),
-        description=_(u'Enter any legal disclaimer and limitations for '
-                      u'downloads that should appear on each page for '
-                      u'dowloadable files.'),
-        default=_(u'Fill in the text for the legal download disclaimer.'),
+        title=_(safe_unicode('Text of the Legal Disclaimer and Limitations for Downlaods')),
+        description=_(safe_unicode('Enter any legal disclaimer and limitations for '
+                      'downloads that should appear on each page for '
+                      'dowloadable files.')),
+        default=_(safe_unicode('Fill in the text for the legal download disclaimer.')),
         required=False,
     )
 
